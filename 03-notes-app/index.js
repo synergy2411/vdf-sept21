6ef22dcca794398051ec4d39f45bf6fa46006163
Sumit K <synergy2411@outlook.com>
2021-09-15 14:28:13 +0530

    command created

--- 03-notes-app/index.js
+++ 03-notes-app/index.js
@@ -1,5 +1,6 @@
 const yargs = require("yargs");
 
+// ADD
 yargs.command({
     command : "add",
     description : "to add new note",
@@ -17,6 +18,49 @@ yargs.command({
     },
     handler : (args) => {
         console.log("Handler Works!", args)
+    }
+})
+
+// READ
+yargs.command({
+    command : "read",
+    description : "to read one note",
+    builder : {
+        title : {
+            type : String,
+            demandOption : true,
+            description : "title to read one note"
+        }
+    },
+    handler : args => {
+        console.log(args)
+    }
+})
+
+// REMOVE
+
+yargs.command({
+    command : "remove",
+    description : "to remove on note",
+    builder : {
+        title : {
+            type : String,
+            demandOption : true,
+            description : "title to remove one note"
+        }
+    },
+    handler : args => {
+        console.log(args)
+    }
+})
+
+// LIST
+
+yargs.command({
+    command : "list",
+    description : "to list down all notes",
+    handler : () => {
+        console.log("Notes coming soon...")
     }
 })
 
