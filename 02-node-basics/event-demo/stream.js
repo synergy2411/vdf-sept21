588f37bfa23697e1fdd35308a1e791eef0598770
Sumit K <synergy2411@outlook.com>
2021-09-16 11:14:27 +0530

    streams added

---
+++ 02-node-basics/event-demo/stream.js
@@ -0,0 +1,32 @@
+// const EventEmitter = require("events").EventEmitter;
+
+// const Stream = require("stream").Stream;
+
+// const readable = new Stream.Readable()
+// const writable = new Stream.Writable()
+
+// console.log(readable instanceof EventEmitter)
+// console.log(writable instanceof EventEmitter)
+
+
+const fs = require("fs");
+const readStream = fs.createReadStream("./app.js")
+const writeStream = fs.createWriteStream("./text.txt")
+readStream.pipe(writeStream)
+
+// readStream.pipe(process.stdout);
+
+readStream.on("data", (chunk) => {
+    console.log(chunk.toString());
+    readStream.on("end", () => {
+        console.log("Finish reading");
+    })
+})
+
+// accessing the FS, and sending file to client
+// Download Server
+// readStream.pipe(response)
+// Upload Server
+// request.pipe(writeStream);
+
+// pipe() |
